a7f373fd5e615b723f0f2dac6e217e8ad9ee768b
Gabu916 <csg.-@hotmail.com>
2022-10-23 21:32:13 -0300

    persona

--- src/main/java/com/portafolio/csg/Controller/CEducacion.java
+++ src/main/java/com/portafolio/csg/Controller/CEducacion.java
@@ -21,8 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-//@CrossOrigin(origins = {"https://frontendcsg.web.app","http://localhost:4200"})
-@CrossOrigin(origins = "https://frontendcsg.web.app")
+@CrossOrigin(origins = {"https://frontendcsg.web.app","http://localhost:4200"})
+//@CrossOrigin(origins = "https://frontendcsg.web.app")
 public class CEducacion {
 
     @Autowired
--- src/main/java/com/portafolio/csg/Controller/CExperiencia.java
+++ src/main/java/com/portafolio/csg/Controller/CExperiencia.java
@@ -21,8 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-//@CrossOrigin(origins = {"https://frontendcsg.web.app","http://localhost:4200"})
-@CrossOrigin(origins = "https://frontendcsg.web.app")
+@CrossOrigin(origins = {"https://frontendcsg.web.app","http://localhost:4200"})
+//@CrossOrigin(origins = "https://frontendcsg.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
